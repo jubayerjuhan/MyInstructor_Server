dc8d08aec1c009d9a9fe34fe7228f44eefe84e6d
Jubayer Juhan <davidjuhan23@gmail.com>
2023-04-02 01:15:49 +0600

    DB URI Changed

--- database/database.js
+++ database/database.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
 export const connectToDatabase = () => {
   try {
     mongoose.connect(
       process.env.NODE_ENV === "production"
         ? process.env.DB_URI
-        : process.env.LOCAL_DB_URI,
+        : process.env.DB_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       },
       () => {
-        console.log("Connected to database...");
+        console.log(`Connected to database... ${process.env.DB_URI}`);
       }
     );
   } catch (error) {
